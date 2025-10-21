8205d2245fdcd54057405698d711368b25bd3301
Olle Ronstad <Olle.Ronstad@gmail.com>
2025-10-21 20:03:30 +0200

    feat: Add DebugUI implementation and utility function for querying entities

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,6 @@
-use crate::common::DecisionChoice;
+use hecs::{Query, QueryMut, World};
+
+use crate::common::{self, DecisionChoice};
 
 #[derive(Clone)]
 pub struct SimpleDecisionChoice {
@@ -15,6 +17,48 @@ impl SimpleDecisionChoice {
             description: description.to_string(),
         }
     }
+}
+
+
+pub struct DebugUI;
+
+impl common::UserStrategy for DebugUI {
+    fn new(state: std::sync::Arc<std::sync::Mutex<hecs::World>>) -> Self
+    where
+        Self: Sized {
+        Self
+    }
+
+    fn get_user_decision(
+        &self,
+        decisions: Vec<Box<dyn DecisionChoice>>,
+    ) -> Box<dyn DecisionChoice> {
+        for decision in &decisions {
+            println!("Decision available: {} - {}", decision.id(), decision.name());
+        }
+
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        // For simplicity, just return the first decision
+
+        decisions[usize::from_str_radix(input.trim(), 10).unwrap()].clone_box()
+    }
+
+    fn send_message(&self, message: String) {
+        println!("Message to user: {}", message);
+    }
+}
+
+/// Utility function to query a single entity from the world
+pub fn query_one_from_world<T: Query>(world: &mut World) -> Option<<T as Query>::Item<'_>> {
+    let mut query = world.query_mut::<T>();
+    query
+        .into_iter()
+        .next()
+        .map(|entity| entity.1)
+
 }
 
 impl DecisionChoice for SimpleDecisionChoice {
