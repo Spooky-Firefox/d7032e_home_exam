491bbba27df64de3c91532aeec9525a8e32d30f7
Olle Ronstad <Olle.Ronstad@gmail.com>
2025-10-21 16:53:14 +0200

    refactor: Update Game structure to use loop_phase and game objects

--- src/game.rs
+++ src/game.rs
@@ -1,50 +1,59 @@
 use std::sync::{Arc, Mutex};
 
-use crate::{common::UserStrategy, util::ProductionDie};
+use crate::{
+    cards::initialize::init_card_position,
+    common::UserStrategy,
+    phase::{
+        loop_phase,
+        roll_dice::{self, DiceRoll},
+    },
+    util::{self, SimpleDecisionChoice},
+};
 use hecs::World;
+use log::info;
 //todo dont use initialize cards, make a better solution later
 use crate::cards::initialize::initialize_cards;
+use crate::game_objects;
 pub struct Game<T: UserStrategy> {
     user_strategy: T,
-    phases: Vec<Box<dyn crate::common::Phase>>,
+    phase: Box<dyn crate::common::Phase>,
     state: Arc<Mutex<World>>,
 }
 
 impl<T: UserStrategy> Game<T> {
     pub fn new() -> Self {
         let state = Arc::new(Mutex::new(World::new()));
-        state.lock().unwrap().spawn((ProductionDie::new(),));
 
+        // TODO make another function to initialize other game components
+        info!("Initializing game components...");
+
+        // TODO move make this less clunky have array of initializers or something
+
+        info!("Initializing cards...");
         initialize_cards(state.clone());
+
+        info!("Initializing card positions...");
+        init_card_position(state.clone());
+
+        info!("initializing game objects...");
+        game_objects::init::initialize_game_objects(state.clone());
         Game {
             user_strategy: T::new(state.clone()),
             state,
-            phases: vec![Box::new(crate::phase::production_die::ProductionDie::new())],
+            phase: Box::new(loop_phase::LoopPhase::new(
+                [Box::new(roll_dice::DiceRoll::new())],
+                [Some([
+                    SimpleDecisionChoice::new(0, "Continue", "Continue to next phase"),
+                    SimpleDecisionChoice::new(1, "Abort", "Abort the game"),
+                ])],
+            )),
         }
-        
     }
 
     pub fn start(&mut self) {
-        // temporary user decision example
         self.user_strategy.send_message("game started".to_string());
-        
-        self.phases.iter_mut().for_each(|phase| {
-            phase.evaluate(
-                self.state.clone(), /* this is a arc, "shallow copy" */
-                &mut self.user_strategy,
-            );
-        });
-        // let _u = self.user_strategy.get_user_decision(
-        //     (0..10)
-        //         .map(|i| {
-        //             Box::new(crate::util::SimpleDecisionChoice {
-        //                 name: format!("Choice {}", i),
-        //                 description: format!("This is choice number {}", i),
-        //             }) as Box<dyn crate::common::DecisionChoice>
-        //         })
-        //         .collect(),
-
-        // );
-        // Game starting logic can be implemented here
+        info!("evaluating base phase...");
+        self.phase
+            .evaluate(self.state.clone(), &mut self.user_strategy);
     }
 }
